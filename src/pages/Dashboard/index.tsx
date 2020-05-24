a23b707d5e1f829c7e411c0a0d7ac97d4c9ce637
Halisson Alves <halisson.eng@gmail.com>
2020-05-24 14:20:58 -0300

    Estilização do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Explore repositórios no Github</h1>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/25910972?s=460&u=f3e860eb8d37b0d225ad551a32a145a777aa388a&v=4"
+            alt="Halisson Alves"
+          />
+          <div>
+            <strong>halissonalves/gameszerados</strong>
+            <p>
+              Website desenvolvido para registrar os jogos e estatísticas de
+              jogos que venho zerando ao longo do tempo.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/25910972?s=460&u=f3e860eb8d37b0d225ad551a32a145a777aa388a&v=4"
+            alt="Halisson Alves"
+          />
+          <div>
+            <strong>halissonalves/gameszerados</strong>
+            <p>
+              Website desenvolvido para registrar os jogos e estatísticas de
+              jogos que venho zerando ao longo do tempo.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/25910972?s=460&u=f3e860eb8d37b0d225ad551a32a145a777aa388a&v=4"
+            alt="Halisson Alves"
+          />
+          <div>
+            <strong>halissonalves/gameszerados</strong>
+            <p>
+              Website desenvolvido para registrar os jogos e estatísticas de
+              jogos que venho zerando ao longo do tempo.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
