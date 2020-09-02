45118dfdf9f748a1af99cb68dd36e981db8af43a
halisson <halisson.amaral@softvision.com.br>
2020-09-02 15:04:50 -0300

    Updates

---
+++ precache-manifest.407b2b0f2b04034e765536b33552b873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02946909c81a491f00f451341061779",
+    "url": "/githubapi/index.html"
+  },
+  {
+    "revision": "813e4fd423f495ecbfd5",
+    "url": "/githubapi/static/js/2.a7b4c221.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubapi/static/js/2.a7b4c221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02434a1a7581eed5ec90",
+    "url": "/githubapi/static/js/main.f5441425.chunk.js"
+  },
+  {
+    "revision": "48e4bee1ef64c2884f26",
+    "url": "/githubapi/static/js/runtime-main.258de5b4.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/githubapi/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/githubapi/static/media/logo.04f6798f.svg"
+  }
+]);
